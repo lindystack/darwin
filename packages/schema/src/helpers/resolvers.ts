0d127215390536e7ffcdfd3902dc7bad2a0b2ac2
Andrew Brown <andrewbrown600@gmail.com>
2023-04-07 15:45:37 -0700

    update: replace resolveSchma with resolveRef

--- packages/schema/src/helpers/resolvers.ts
+++ packages/schema/src/helpers/resolvers.ts
@@ -1,34 +1,14 @@
-import { JsonSchema } from "../types/JsonSchema";
-import { hasProperty, has$Ref, has$Defs, isSelfRef } from "./guards";
+import { JsonSchema } from "../schemas";
+import { Option, fromNullable, map as mapOption } from "fp-ts/Option";
+import { pipe } from "fp-ts/function";
 
-export function resolveSchema(key: string, schema: JsonSchema): JsonSchema {
-	if (!hasProperty(key, schema)) {
-		throw new Error(`Schema does not have property ${key}`);
-	}
-
-	let prop = schema.properties[key];
-
-	if (has$Ref(prop)) {
-		if (isSelfRef(prop)) {
-			return schema;
-		}
-
-		if (!has$Defs(schema)) {
-			throw new Error("Schema does not have $defs");
-		}
-
-		if (!has$Ref(prop)) {
-			throw new Error("Schema does not have $ref");
-		}
-
-		let $ref = prop.$ref;
-
-		if (!($ref in schema.$defs)) {
-			throw new Error(`Schema does not have $defs for ${$ref}`);
-		}
-
-		return schema.$defs[$ref];
-	}
-
-	return prop;
-}
+// This function takes a schema and a $ref, and returns the resolved schema as an Option
+export const resolveRef = (
+	schema: JsonSchema,
+	ref: string,
+): Option<JsonSchema> => {
+	return pipe(
+		fromNullable(schema.$defs),
+		mapOption((defs) => defs[ref]),
+	);
+};
