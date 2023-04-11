b22c0171b76537afb22e3b9bb751f2e7c88036ab
Andrew Brown <andrewbrown600@gmail.com>
2023-04-10 18:22:17 -0700

    tests

--- packages/schema/test/helpers.test.ts
+++ packages/schema/test/helpers.test.ts
@@ -1,39 +1,47 @@
 import { test, expect, assert } from "vitest";
 
-import { ObjectSchema } from "..";
-import { isOneToMany, hasProperties, isRef } from "..";
+import { ObjectSchema } from "../src";
+import { isOneToMany, hasProperties, isRef } from "../src";
 
 export const flipEntitySchema: ObjectSchema = {
-  $id: "/schemas/leadgen/flip_entity",
-  type: "object",
-  title: "FlipEntity",
-  properties: {
-    id: { type: "integer", title: "Id" },
-    name: { type: "string", title: "Name" },
-    parent: { $ref: "#", title: "Parent" },
-    onboardingStatus: { type: "string", title: "Onboarding Status", enum: ["foo", "bar", "baz"] },
-    researchStatus: { type: "string", title: "Research Status", enum: ["foo", "bar", "baz"] },
-    assignedTo: { $ref: "/schemas/core/users", title: "Assigned To" },
-    contacts: {
-      type: "array",
-      title: "Contacts",
-      items: { $ref: "/schemas/leadgen/flip_entity_contact", title: "Contact" },
-    },
-    followUpBy: { type: "string", title: "Follow Up By", format: "date" },
-    tags: {
-      type: "array",
-      title: "Tags",
-      items: {
-        type: "string",
-        enum: ["foo", "bar", "baz"],
-      },
-    },
-  },
+	$id: "/schemas/leadgen/flip_entity",
+	type: "object",
+	title: "FlipEntity",
+	properties: {
+		id: { type: "integer", title: "Id" },
+		name: { type: "string", title: "Name" },
+		parent: { $ref: "#", title: "Parent" },
+		onboardingStatus: {
+			type: "string",
+			title: "Onboarding Status",
+			enum: ["foo", "bar", "baz"],
+		},
+		researchStatus: {
+			type: "string",
+			title: "Research Status",
+			enum: ["foo", "bar", "baz"],
+		},
+		assignedTo: { $ref: "/schemas/core/users", title: "Assigned To" },
+		contacts: {
+			type: "array",
+			title: "Contacts",
+			items: { $ref: "/schemas/leadgen/flip_entity_contact", title: "Contact" },
+		},
+		followUpBy: { type: "string", title: "Follow Up By", format: "date" },
+		tags: {
+			type: "array",
+			title: "Tags",
+			items: {
+				type: "string",
+				enum: ["foo", "bar", "baz"],
+			},
+		},
+	},
 };
 
 test("test", () => {
-  assert(hasProperties(flipEntitySchema));
-  expect(isOneToMany(flipEntitySchema.properties.tags)).toBe(false);
-  expect(isOneToMany(flipEntitySchema.properties.contacts)).toBe(true);
-  expect(isRef(flipEntitySchema.properties.assignedTo)).toBe(true);
+	assert(hasProperties(flipEntitySchema));
+	expect(isOneToMany(flipEntitySchema.properties.tags)).toBe(false);
+	expect(isOneToMany(flipEntitySchema.properties.contacts)).toBe(true);
+	expect(isRef(flipEntitySchema.properties.assignedTo)).toBe(true);
 });
--- packages/schema/test/resolvers.test.ts
+++ packages/schema/test/resolvers.test.ts
@@ -1,6 +1,7 @@
-import { test, expect, assert } from "vitest";
+import { test, expect, describe, it } from "vitest";
 
-import { resolveRef } from "../src/helpers/resolvers";
+import { resolveRef, resolveProperty } from "../src/helpers/resolvers";
+import { JsonSchema } from "../src/schemas";
 
 // rome-ignore lint/suspicious/noExplicitAny: testing
 export const flipEntitySchema: any = {
@@ -11,10 +12,93 @@ export const flipEntitySchema: any = {
 		},
 	},
 };
+describe("resolveRef", () => {
+	test("resolveRef", () => {
+		const result = resolveRef(flipEntitySchema, "/schemas/core/users");
 
-test("test", () => {
-	const result = resolveRef(flipEntitySchema, "/schemas/core/users");
+		// @ts-expect-error
+		expect(result.value).toEqual({ type: "object", title: "User" });
+	});
+});
 
-	// @ts-expect-error
-	expect(result.value).toEqual({ type: "object", title: "User" });
+describe("resolveProperty", () => {
+	const schema: JsonSchema = {
+		type: "object",
+		properties: {
+			foo: {
+				type: "object",
+				properties: {
+					bar: { type: "string" },
+				},
+			},
+			qux: { $ref: "schemas/qux" },
+			quux: { $ref: "schemas/quux" },
+		},
+		$defs: {
+			"schemas/qux": {
+				type: "string",
+				$comment: "qux",
+			},
+			"schemas/quux": {
+				type: "object",
+				format: "corge",
+				properties: {
+					corge: { $ref: "schemas/grault" },
+				},
+			},
+			"schemas/grault": {
+				type: "string",
+				$comment: "grault",
+			},
+		},
+	};
+
+	test("a valid key and root schema", () => {
+		it("returns the resolved schema", () => {
+			const result = resolveProperty("foo.bar", schema);
+
+			expect(result).toEqual({ type: "string" });
+		});
+	});
+
+	test("an invalid key", () => {
+		it("returns None", () => {
+			const result = resolveProperty("baz", schema);
+
+			expect(result).toBeUndefined();
+		});
+	});
+
+	test("a key that resolves to nested object", () => {
+		it("returns the resolved schema", () => {
+			const result = resolveProperty("foo", schema);
+
+			expect(result).toEqual({
+				type: "object",
+				properties: { bar: { type: "string" } },
+			});
+		});
+	});
+
+	test("a key that resolves to a $ref", () => {
+		it("returns the resolved schema", () => {
+			const result = resolveProperty("qux", schema);
+
+			expect(result).toEqual({
+				type: "string",
+				$comment: "qux",
+			});
+		});
+	});
+
+	test("a key that resolves to a nested $ref", () => {
+		it("returns the resolved schema", () => {
+			const result = resolveProperty("quux", schema);
+
+			expect(result).toEqual({
+				type: "string",
+				$comment: "grault",
+			});
+		});
+	});
 });
